c01aa3635fbe5fa14c75c53596e890644b027111
Raphael Ickovics <raphealickovics@gmail.com>
2021-09-22 16:11:31 +0300

    modified client properties a bit

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include "project_exception.hpp"
+#include "project_status.hpp"
 
 int main(void)
 {
-    std::cout << "Hello World!\n";
+    try
+    {
+        
+    }
+    catch (const ProjectException& e)
+    {
+        std::cout << "Caught ProjectException: (status=" << static_cast<std::underlying_type_t<ProjectStatus>>(e.status) << ").\n";
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Caught std::exception: (e.what=" << e.what() << ").\n";
+    }
+    catch (...)
+    {
+        std::cout << "Caught unknown exception.\n";
+    }
 }
 
