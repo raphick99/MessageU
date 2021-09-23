83c56bbfb9eb2be50004cb04fd797b03091e0633
Raphael Ickovics <raphealickovics@gmail.com>
2021-09-23 08:22:02 +0300

    basic main loop

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <limits>
 #include <boost/asio.hpp>
 #include "project_exception.hpp"
 #include "project_status.hpp"
@@ -7,7 +8,29 @@ int main(void)
 {
     try
     {
+        std::cout << "MessageU client at your service.\n\n";
+        while (1)
+        {
+            int input;
+            std::cout << "10) Register\n";
+            if (!(std::cin >> input))
+            {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "invalid input. not an integer\n";
+                continue;
+            }
 
+            switch (input)
+            {
+            case 10:
+                std::cout << "in correct input\n";
+                break;
+            default:
+                std::cout << "invalid input. not a valid option\n";
+                continue;
+            }
+        }
     }
     catch (const ProjectException& e)
     {
