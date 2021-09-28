e1c25b259c7803ebca7f7aaf735923944dc315f0
Raphael Ickovics <raphealickovics@gmail.com>
2021-09-28 21:39:07 +0300

    Made Database functions be thread-safe

--- server/database.py
+++ server/database.py
@@ -1,12 +1,22 @@
+import threading
 import sqlite3
 from datetime import datetime
 import exceptions
 
 
 DATABASE_FILE = 'server.db'
+DATABASE_LOCK = threading.RLock()
+
+
+def multithread_protect(func):
+    def wrapper(*args, **kwargs):
+        with DATABASE_LOCK:
+            return func(*args, **kwargs)
+    return wrapper
 
 
 class Database:
+    @multithread_protect
     def __init__(self):
         self.connection = sqlite3.connect(DATABASE_FILE)
         self.connection.execute(
@@ -26,10 +36,12 @@ class Database:
         )
         self.connection.commit()
 
+    @multithread_protect
     def add_message(self, to_client, from_client, type, content):
         # todo need to update client last seen
         pass
 
+    @multithread_protect
     def add_client(self, client_id, name, public_key):
         try:
             self.connection.execute(
@@ -40,9 +52,11 @@ class Database:
             raise exceptions.ClientWithRequestedNameAlreadyRegistered(name)
         self.connection.commit()
 
+    @multithread_protect
     def get_client_list(self):
         return self.connection.execute('SELECT ID, Name FROM clients').fetchall()
 
+    @multithread_protect
     def get_public_key_by_client_id(self, client_id):
         return self.connection.execute('SELECT PublicKey FROM clients WHERE ID = ?', (client_id, )).fetchone()
 
