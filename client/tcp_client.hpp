6a3e624fac5ce6e08bac2ee1d1e16f5a9fcf6386
Raphael Ickovics <raphealickovics@gmail.com>
2021-10-03 13:50:22 +0300

    moved some code to the cpp file

--- client/tcp_client.hpp
+++ client/tcp_client.hpp
@@ -10,28 +10,10 @@ public:
 	TcpClient(const std::string& host, const std::string& port);
 	~TcpClient() = default;
 
-	std::string read_string(size_t buffer_size)
-	{
-		if (buffer_size == 0)
-		{
-			return std::string();
-		}
+	std::string read_string(size_t);
+	void write_string(const std::string&);
 
-		std::string buffer;
-		buffer.resize(buffer_size);
-
-		std::size_t reply_length = boost::asio::read(socket, boost::asio::buffer(buffer.data(), buffer_size));
-		if (reply_length == 0)
-		{
-			throw ProjectException(ProjectStatus::TcpClient_SocketClosedAtOtherEndpoint);
-		}
-		if (reply_length != buffer_size)
-		{
-			throw ProjectException(ProjectStatus::TcpClient_IncompleteRead);
-		}
-		return buffer;
-	}
-
+	// templated function, must be in header
 	template<typename T>
 	T read_struct()
 	{
@@ -46,15 +28,6 @@ public:
 			throw ProjectException(ProjectStatus::TcpClient_IncompleteRead);
 		}
 		return t;
-	}
-
-	void write_string(const std::string& string_to_write)
-	{
-		std::size_t write_length = boost::asio::write(socket, boost::asio::buffer(string_to_write.data(), string_to_write.size()));
-		if (write_length != string_to_write.size())
-		{
-			throw ProjectException(ProjectStatus::TcpClient_IncompleteWrite);
-		}
 	}
 
 	template<typename T>
